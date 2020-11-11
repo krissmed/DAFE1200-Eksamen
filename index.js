26027b9f198dd78e9d5d416b804cac05f4456eb8
krissmed <29783486+krissmed@users.noreply.github.com>
2020-11-11 23:31:14 +0100

    Fikset breakpoint for siteMenu. Animasjon på hamburgermeny er fortsatt WIP

--- index.js
+++ index.js
@@ -54,14 +54,27 @@ function selectActiveSite() {
 
 // Gather html elements
 var headerMenu = document.getElementById("headerMenu");
+var line1 = document.getElementById("line1");
+var line2 = document.getElementById("line2");
+var line3 = document.getElementById("line3");
 
 // Function which toggles hamburgermenu on/off
 function toggleMenu(){
+
+
     if(headerMenu.style.display == "flex"){
         headerMenu.style.display = "";
+        
+        line1.style.transform = "rotate(0deg)"
+        line1.style.opacity = "0%"
+        line3.style.transform = "rotate(0deg)"
     }
     else if (headerMenu.style.display === ""){
         headerMenu.style.display = "flex";
+        
+        line1.style.transform = "rotate(-45deg)"
+        line1.style.opacity = "100%"
+        line3.style.transform = "rotate(45deg)"
     }
 
     console.log("Toggle Hamburger Menu") // Troubleshoot 
