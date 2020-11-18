63702312373a4a97924323a066eaaa4882a4a188
Ole Jørgen Knoph <s354371@oslomet.no>
2020-11-18 17:27:02 +0100

    Fikset source.html og deler av bilder challenge og index

--- assets/JavaScript/source.js
+++ assets/JavaScript/source.js
@@ -8,11 +8,10 @@ let groupPage3 = document.getElementsByClassName("Group3")[0];
 
     /*Makes the first page active without the user doing anything*/
     if (window.scrollY >= 0) {
-        contentPage.classList.add("activeSitenav");
-        console.log("content ved 0");
-        imgPage1.classList.remove("activeSitenav");
-        meetingPage2.classList.remove("activeSitenav");
-        groupPage3.classList.remove("activeSitenav");
+        contentPage.classList.add("siteNavDisplayed");
+        imgPage1.classList.remove("siteNavDisplayed");
+        meetingPage2.classList.remove("siteNavDisplayed");
+        groupPage3.classList.remove("siteNavDisplayed");
     }
 
     /*when the windown scrolls, the functions starts*/
@@ -24,35 +23,35 @@ let groupPage3 = document.getElementsByClassName("Group3")[0];
             /*if the window is scrolled down further than 3.9 of the height of the browser (390vh)*/
             if (window.scrollY > window.innerHeight * 3.90) {
                     /*adds the .activeSitenav class to the group-siteNav*/
-                groupPage3.classList.add("activeSitenav");
+                groupPage3.classList.add("siteNavDisplayed");
 
                     /*Removes from other so just 1 is active*/
-                contentPage.classList.remove("activeSitenav");
-                meetingPage2.classList.remove("activeSitenav");
-                imgPage1.classList.remove("activeSitenav");
+                contentPage.classList.remove("siteNavDisplayed");
+                meetingPage2.classList.remove("siteNavDisplayed");
+                imgPage1.classList.remove("siteNavDisplayed");
             }
             /*if the window is scrolled down further than 2.9 of the height of the browser (290vh)*/
             else if (window.scrollY > window.innerHeight * 2.90) {
-                meetingPage2.classList.add("activeSitenav");
+                meetingPage2.classList.add("siteNavDisplayed");
 
-                contentPage.classList.remove("activeSitenav");
-                imgPage1.classList.remove("activeSitenav");
-                groupPage3.classList.remove("activeSitenav");
+                contentPage.classList.remove("siteNavDisplayed");
+                imgPage1.classList.remove("siteNavDisplayed");
+                groupPage3.classList.remove("siteNavDisplayed");
             }
             /*if the window is scrolled down further than 1.5 of the height of the browser (150vh)*/
             else if (window.scrollY > window.innerHeight * 1.5) {
-                imgPage1.classList.add("activeSitenav");
+                imgPage1.classList.add("siteNavDisplayed");
 
-                contentPage.classList.remove("activeSitenav");
-                meetingPage2.classList.remove("activeSitenav");
-                groupPage3.classList.remove("activeSitenav");
+                contentPage.classList.remove("siteNavDisplayed");
+                meetingPage2.classList.remove("siteNavDisplayed");
+                groupPage3.classList.remove("siteNavDisplayed");
             }
             /*if the user scroll back to top*/
             else if (window.scrollY > -5) {
-                contentPage.classList.add("activeSitenav");
+                contentPage.classList.add("siteNavDisplayed");
 
-                imgPage1.classList.remove("activeSitenav");
-                meetingPage2.classList.remove("activeSitenav");
-                groupPage3.classList.remove("activeSitenav");
+                imgPage1.classList.remove("siteNavDisplayed");
+                meetingPage2.classList.remove("siteNavDisplayed");
+                groupPage3.classList.remove("siteNavDisplayed");
             }
         }
