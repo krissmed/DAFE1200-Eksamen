8d032ea7763d648047f2ef396016a9746126d423
krissmed <29783486+krissmed@users.noreply.github.com>
2020-11-29 16:32:49 +0100

    #152

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,14 @@
+var appImg1 = document.getElementById("appImg1");
+var appImg2 = document.getElementById("appImg2");
+var appImg3 = document.getElementById("appImg3");
+var appImg4 = document.getElementById("appImg4");
+var appImg5 = document.getElementById("appImg5");
+var appImg6 = document.getElementById("appImg6");
+var appImg7 = document.getElementById("appImg7");
+var appImg8 = document.getElementById("appImg8");
+function enlargeImg(img){
+    if(img == "1"){
+        console.log("Bilde 1");
+        appImg1.style.transform = "scale(2,2)";
+    }
+}
