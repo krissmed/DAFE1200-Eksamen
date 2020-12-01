c3b3837d931c97d44847ad71c8c3144143df7ab5
Ole Jørgen Knoph <s354371@oslomet.no>
2020-12-01 18:04:19 +0100

    Ferdig oversettelse

--- assets/JavaScript/indexSiteNav.js
+++ assets/JavaScript/indexSiteNav.js
@@ -12,23 +12,36 @@ let sideNav2 = document.getElementsByClassName("sideNavElements 2")[0];
 let sideNav3 = document.getElementsByClassName("sideNavElements 3")[0];
 let sideNav4 = document.getElementsByClassName("sideNavElements 4")[0];
 let sideNav5 = document.getElementsByClassName("sideNavElements 5")[0];
+let sideNav6 = document.getElementsByClassName("sideNavElements 6")[0];
 
 /*Makes sure the HOME-link is active at first even when user is not scrolling*/
 if (window.scrollY >= 0) {
-
     sideNav1.classList.add("sideNavActive");
 
     sideNav2.classList.remove("sideNavActive");
     sideNav3.classList.remove("sideNavActive");
     sideNav4.classList.remove("sideNavActive");
     sideNav5.classList.remove("sideNavActive");
+    sideNav6.classList.remove("sideNavActive");
 }
 
 //Function that chooses what Site is active based on where the user is scrolling on the site*/
 function selectActiveSite() {
     /*if the window is scrolled down furter than 390vh (3.9times the screen height)*/
-    if (window.scrollY >= window.innerHeight * 3.9) {
+    if (window.scrollY >= window.innerHeight * 4.9) {
+        /*active class added app*/
+        sideNav6.classList.add("sideNavActive");
 
+        /*active class removed from the others so its only 1 link active*/
+        sideNav4.classList.remove("sideNavActive");
+        sideNav3.classList.remove("sideNavActive");
+        sideNav2.classList.remove("sideNavActive");
+        sideNav1.classList.remove("sideNavActive");
+        sideNav5.classList.remove("sideNavActive");
+
+    }
+    /*if the window is scrolled down furter than 390vh (3.9times the screen height)*/
+    if (window.scrollY >= window.innerHeight * 3.9 && window.scrollY < window.innerHeight * 4.9) {
         /*active class added app*/
         sideNav5.classList.add("sideNavActive");
 
@@ -37,11 +50,11 @@ function selectActiveSite() {
         sideNav3.classList.remove("sideNavActive");
         sideNav2.classList.remove("sideNavActive");
         sideNav1.classList.remove("sideNavActive");
+        sideNav6.classList.remove("sideNavActive");
 
     }
     /*if the window is scrolled down furter than 290vh (2.9times the screen height)*/
     if (window.scrollY > window.innerHeight * 2.9 && window.scrollY < window.innerHeight * 3.9) {
-
         /*active class added app*/
         sideNav4.classList.add("sideNavActive");
 
@@ -51,36 +64,40 @@ function selectActiveSite() {
         sideNav2.classList.remove("sideNavActive");
         sideNav1.classList.remove("sideNavActive");
         sideNav5.classList.remove("sideNavActive");
+        sideNav6.classList.remove("sideNavActive");
     }
     /*if the window is scrolled down furter than 190vh (1.9times the screen height)*/
     else if (window.scrollY > window.innerHeight * 1.9 && window.scrollY < window.innerHeight * 2.9) {
-
         sideNav3.classList.add("sideNavActive");
 
         sideNav1.classList.remove("sideNavActive");
         sideNav2.classList.remove("sideNavActive");
         sideNav4.classList.remove("sideNavActive");
         sideNav5.classList.remove("sideNavActive");
+        sideNav6.classList.remove("sideNavActive");
+
 
     }
     /*if the window is scrolled down furter than 90vh (0.9times the screen height)*/
     else if (window.scrollY > window.innerHeight * 0.9 && window.scrollY < window.innerHeight * 1.9) {
-
         sideNav2.classList.add("sideNavActive");
 
         sideNav1.classList.remove("sideNavActive");
         sideNav3.classList.remove("sideNavActive");
         sideNav4.classList.remove("sideNavActive");
         sideNav5.classList.remove("sideNavActive");
+        sideNav6.classList.remove("sideNavActive");
+
     }
     /*if the window is scrolled down furter than -100vh (-1times the screen height) Just to be sure the class is added at top*/
     else if (window.scrollY > -1 && window.scrollY < window.innerHeight * 0.9) {
-
         sideNav1.classList.add("sideNavActive");
 
         sideNav2.classList.remove("sideNavActive");
         sideNav3.classList.remove("sideNavActive");
         sideNav4.classList.remove("sideNavActive");
         sideNav5.classList.remove("sideNavActive");
+        sideNav6.classList.remove("sideNavActive");
+
     }
 }
